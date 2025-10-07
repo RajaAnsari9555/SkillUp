8a85095237e98d0953b323d939ced49425d9b684
MD Raja Ansari <mdrajaansari9555@gmail.com>
2025-10-07 18:40:29 +0530

    Update ReviewPage.jsx

--- Fronted/src/component/ReviewPage.jsx
+++ Fronted/src/component/ReviewPage.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import ReviewCard from './reviewCard'
+import ReviewCard from './ReviewCard'
 
 const ReviewPage = () => {
 
